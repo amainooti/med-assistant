b49d482f70765ef9b26b5ae31f9e64ea0929dc56
dhanzy <holuwadanzy@gmail.com>
2022-08-19 22:45:05 +0100

    Added dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const colors = require("colors")
+const colors = require("colors");
 
 const PORT = process.env.PORT || 8080;
 
-
 // inbuilt middleware
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // custom middleware
-app.use("/api/userData", require("./routes/userRoute"))
+app.use("/api/userData", require("./routes/userRoute"));
+
 // app.use("/api/userDashboard", require("./routes/userDashBoard"))
 
 app.listen(PORT, () => {

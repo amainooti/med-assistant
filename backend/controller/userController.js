dd4b807c28be28bdeb682349edd4d368bc561a58
amainooti <amainooti@gmail.com>
2022-08-19 23:52:34 +0100

    fixed problem

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,7 @@
+
 const User = require("../models/userModel")
+
+
 // controller for users data
 
 // @method: GET
@@ -6,6 +9,8 @@ const getUser = async (req, res)=>{
     const user = await User.find()
     res.status(200).json(user)
 }
+
+// @method: POST
 const setUser = async (req, res)=>{
 
     // const user = await User.create({
@@ -16,11 +21,15 @@ const setUser = async (req, res)=>{
     // })
     res.status(200).json({message: "Testing user"})
 }
+
+// @method: PUT
 const updateUser = (req, res)=>{
     res.status(200).json({
         message: `Updating user data ${req.params.id}`
     })
 }
+
+// @method: DELETE
 const deleteUser = (req, res)=>{
     res.status(200).json({
         message: `Deleting user data ${req.params.id}`

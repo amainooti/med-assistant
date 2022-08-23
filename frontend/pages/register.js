39c0d23a5966e68c5bbdb5d7e487c814166aa13a
James Onyekachi <29162889+okilonet1@users.noreply.github.com>
2022-08-23 22:16:51 +0100

    Case Sensitive shit

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AuthLayout from "../components/layout/AuthLayout";
+import AuthLayout from "../components/Layout/AuthLayout";
 import ProfileForm from "../components/Auth/ProfileForm";
 import RegisterForm from "../components/Auth/RegisterForm";
 
